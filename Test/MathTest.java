7de651381ed36b204235e4e89c9f1449020242e8
wirga3012 <wirgaile@gmail.com>
2023-03-31 15:38:50 +0300

    JUnits

--- Test/MathTest.java
+++ Test/MathTest.java
@@ -4,6 +4,18 @@ import static org.junit.Assert.*;
 
 public class MathTest{
     @Test
+    public void MathRoundTest(){
+        assertEquals(31,Math.round(30.4556565));
+    }
+    @Test
+    public void MathMaxTest(){
+        assertEquals(120,Math.max(120,150));
+        assertEquals(1,5);
+    }
+    @Test
+    public void MathDecrementTest(){
+        assertEquals(653254,);
+    }
 
 
 
